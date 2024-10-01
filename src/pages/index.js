520348e66eef837e437c8d717658ac9541488b44
Okera Johnson <okerajohnson@gmail.com>
2024-10-01 12:19:28 -0400

    ndecks

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,16 @@
 import * as React from "react"
 
-export default function Home() {
-  return <div>Hello world!</div>
+// Definie component
+
+const IndexPage = () => {
+  return (
+    <main>
+      <h1>Welcome to wayne world</h1>
+      <p>Im making this by following lord gyats turotial</p>
+    </main>
+  )
 }
+
+export const Head = () => <title>Home Page</title>
+
+export default IndexPage
