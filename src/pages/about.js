f5b3fd03439fe8d1214d5c30e30aabce9577ef95
Okera Johnson <okerajohnson@gmail.com>
2024-10-01 12:26:51 -0400

    more green

---
+++ src/pages/about.js
@@ -0,0 +1,14 @@
+import * as React from 'react'
+
+const AboutPage = () => {
+    return (
+        <main>
+            <h1>About Me</h1>
+            <p>Hi there i made this lil website</p>
+        </main>
+    )
+}
+
+export const Head = () => <title>ABout Me</title>
+
+export default AboutPage
