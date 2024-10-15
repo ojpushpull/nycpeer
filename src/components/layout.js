4145c3a460f567826f3a654f2a7d17dff4109ef9
Okera Johnson <okerajohnson@gmail.com>
2024-10-15 11:36:09 -0400

    graphql query

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,5 +1,5 @@
 import * as React from 'react'
-import { Link } from 'gatsby'
+import { Link, useStaticQuery, graphql } from 'gatsby'
 import {  container,
     heading,
     navLinks,
@@ -7,8 +7,19 @@ import {  container,
     navLinkText } from './layout.module.css'
 
 const Layout = ({ pageTitle, children }) => {
+
+    const data = useStaticQuery(graphql`
+    query {
+        site{
+            siteMetadata{
+                title
+            }
+        }
+    }
+    ` )
     return (
         <div className={container}>
+            <header>{data.site.siteMetadata.title}</header>
             <nav>
                 <ul className={navLinks}>
                     <li className={navLinkItem}><Link to="/" className={navLinkText}>Home</Link> </li>
