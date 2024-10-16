3c727c0c7350d10346965530b7dcb8f5a25aff8d
Okera Johnson <okerajohnson@gmail.com>
2024-10-16 14:44:16 -0400

    mdx

--- src/pages/blog.js
+++ src/pages/blog.js
@@ -1,14 +1,32 @@
 import * as React from 'react'
 import Layout from '../components/layout'
 import Seo from '../components/seo'
+import { graphql } from 'gatsby'
 
-const BlogPage = () => {
+const BlogPage = ({ data }) => {
     return (
         <Layout pageTitle="My Blog Posts">
-            <p> Ill add content here</p>
+            <ul>
+                {
+                    data.allFile.nodes.map(node => (
+                        <li key={node.name}>
+                            {node.name}
+                        </li>
+                    ))
+                }
+            </ul>
         </Layout>
     )
 }
+
+export const query = graphql`
+    query {
+        allFile(filter: {sourceInstanceName: {eq: "blog"}}) {
+            nodes{
+                name
+            }
+        }
+    }`
 
 export const Head = () => <Seo title="My Blog Posts" />
 
